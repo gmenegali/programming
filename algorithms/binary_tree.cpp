797da7f64b8deb943716aaa8d8de2ed0290e81a7
gamenegali <guilherme.menegali@usp.br>
2020-09-23 20:52:23 -0300

    Interesting problem with pointers - Watch out with references to pointers

--- algorithms/binary_tree.cpp
+++ algorithms/binary_tree.cpp
@@ -8,8 +8,9 @@ struct Node {
 	int value;
 	Node *l;
 	Node *r;
-	Node() : l(nullptr), r(nullptr) {} 
-	Node(int x) : value(x), l(nullptr), r(nullptr) {}
+	Node *father;
+	Node() : l(nullptr), r(nullptr), father(nullptr) {} 
+	Node(int x) : value(x), l(nullptr), r(nullptr), father(nullptr) {}
 };
 
 struct Tree{
@@ -17,15 +18,31 @@ struct Tree{
 	Tree() : root(nullptr) {}
 };
 
-void add_nodde(Node *&n, int value){
+void add_node(Node *&n, int value){
+	Node *newNode = new Node(value);
+
 	if(n == nullptr){
-		n = new Node(value);
+		n = newNode;
 	}
 	else{
-		if(n->value >= value)
-			return add_nodde(n->l, value);
-		else
-			return add_nodde(n->r, value);
+		if(n->value > value){
+			if(n->l != nullptr){
+				add_node(n->l, value);
+			}
+			else{
+				n->l = newNode;
+				n->l->father = n;
+			}
+		}
+		else if(n->value < value){
+			if(n->r != nullptr){
+				add_node(n->r, value);
+			}
+			else{
+				n->r = newNode;
+				n->r->father = n;
+			}
+		}
 	}
 }
 
@@ -37,98 +54,119 @@ bool is_leaf(Node *&target){
 	return is_leaf;
 }
 
+Node * biggest_leaf_from_left(Node *&target){
+	if(is_leaf(target)){
+		return target;
+	}
 
-// void handle_remove(Node *&target){
-// 	if(is_leaf(target)){
-// 		delete target;
-// 	}
-// 	else{
-// 		Node *sucessor, *sucessor_father;
+	if(target->r != nullptr){
+		return biggest_leaf_from_left(target->r);
+	}
 
-// 		//Biggest from the right
-// 		if(target->r != nullptr){
-// 			sucessor_father = target;
-// 			sucessor = target->r;
-// 			while(sucessor->l != nullptr){
-// 				sucessor_father = sucessor;
-// 				sucessor = sucessor->l;
-// 			}
-// 		}
+	return nullptr;
+}
 
-// 		//Smallest from the left
-// 		else if(target->l != nullptr){
-// 			sucessor_father = target;
-// 			sucessor = target->l;
-// 			while(sucessor->r != nullptr){
-// 				sucessor_father = sucessor;
-// 				sucessor = sucessor->r;
-// 			}
-// 		}
+Node * smallest_leaf_from_right(Node *&target){
+	if(is_leaf(target)){
+		return target;
+	}
 
-// 		while(sucessor->l != nullptr or sucessor->r != nullptr){
-// 			sucessor_father = sucessor;
-// 			if(sucessor->r != nullptr){
-// 				father_pointer = 'r';
-// 				sucessor = sucessor->r;
-// 			}
-// 			else{
-// 				father_pointer = 'l';
-// 				sucessor = sucessor->l;
-// 			}
-// 		}
+	if(target->l != nullptr){
+		return smallest_leaf_from_right(target->l);
+	}
 
-// 		target->value = sucessor->value;
-// 		if(father_pointer == 'r')
-// 			sucessor_father->r = nullptr;
-// 		if(father_pointer == 'l')
-// 			sucessor_father->l = nullptr;
+	return nullptr;
+}
 
-// 		delete sucessor;
-// 	}
-// }
+char father_direction(Node *&target){
+	Node *parent = target->father;
+	if(parent->l == target)
+		return 'l';
+	else if(parent->r == target)
+		return 'r';
+	else return '-';
+}
 
-// void remove_nodde(Node *&root, int value){
-// 	Node *target;
-// 	if(root->value > value){
-// 		target = root->l;
-// 		if(target != nullptr){
-// 			if(root->l->value != value){
-// 				return remove_nodde(target, value);
-// 			}
-// 			if(is_leaf(target)){
-// 				root->r = nullptr;
-// 			}
-// 			handle_remove(target);			
-// 		}
-// 		else
-// 			cout << "Value not found." << endl;
-// 	}
-// 	else if(root->value < value){
-// 		target = root->r;
-// 		if(target != nullptr){
-// 			if(root->r->value != value){
-// 				return remove_nodde(target, value);
-// 			}
-// 			if(is_leaf(target)){
-// 				root->r = nullptr;
-// 			}
-// 			handle_remove(target);
-// 		}
-// 		else
-// 			cout << value << " not found. Could not be removed." << endl;
+void handle_remove(Tree &t, Node *target){
+	//If is leaf -> Delete and put a fix the father pointer
+	//Else choose between the smallest from the right or the biggest from the left
+	Node *parent = target->father;
+	cout << &(parent->r) << endl;
+	cout << &target << endl;
 
-// 	}
-// 	else //In case the root is the target
-// 		return remove_nodde(root, value);
-// }
 
-// Node *sucessor(Node *&root){
+	char parent_direction = father_direction(target);
 
-// }
+	if(is_leaf(target)){
+		if(parent_direction == 'l'){
+			parent->l = nullptr;
+		}
+		else{
+			parent->r = nullptr;
+		}
+		delete target;
+		return;
+	} 
+	else{
+		Node *sucessor;
+		if(target->l != nullptr){
+			sucessor = biggest_leaf_from_left(target->l);
+		}
+		else if(target->r != nullptr){
+			sucessor = smallest_leaf_from_right(target->r);
+		}
 
-// Node *antecessor(){
-	
-// }
+		if(sucessor != target->l){
+			sucessor->l = target->l;
+			sucessor->l->father = sucessor;
+		}
+		if(sucessor != target->r){
+			sucessor->r = target->r;
+			sucessor->r->father = sucessor;
+		}
+		if(father_direction(sucessor) == 'r'){
+			parent->r = nullptr;
+		}
+		else if(father_direction(sucessor) == 'l'){
+			parent->l = nullptr;
+		}
+		sucessor->father = parent;
+
+		// // Node *parent = target->father;
+		// cout << target << endl;
+		// cout << parent << endl;
+		// cout << parent->r << endl;
+		// cout << sucessor << endl << endl;
+		// parent->r = sucessor;
+		// cout << target << endl;
+		// cout << parent << endl;
+		// cout << parent->r << endl;
+		// cout << sucessor << endl;
+		
+		// if(father_direction(target) == 'r'){
+		// 	parent->r = sucessor;
+		// }
+		// else if(father_direction(target) == 'l'){
+		// 	parent->l = sucessor;
+		// }
+		delete target;
+	}
+
+}
+
+void remove_node(Tree &t, Node *&root, int value){
+	if(root->value == value){
+		return handle_remove(t, root);
+	}
+
+	if(root->value > value){
+		return remove_node(t, root->l, value);
+	}
+
+	if(root->value < value){
+		return remove_node(t, root->r, value);
+	}
+}
 
 void clear_tree(Node *n){
 	if(n->l != nullptr)
@@ -146,37 +184,20 @@ void print_tree(Node *n){
 	cout << n->value << endl;
 }
 
-
 int main(){
 	Tree t;
-	add_nodde(t.root, 25);
-	add_nodde(t.root, 14);
-	add_nodde(t.root, 36);
-	add_nodde(t.root, 3);
-	add_nodde(t.root, 17);
-	add_nodde(t.root, 20);
-	add_nodde(t.root, 19);
-	add_nodde(t.root, 21);
-	print_tree(t.root);
-	cout << endl;
-	remove_nodde(t.root, 21);
-	print_tree(t.root);
-	cout << endl;
-	remove_nodde(t.root, 20);
-	print_tree(t.root);
-	cout << endl;
-	remove_nodde(t.root, 14);
-	print_tree(t.root);
-	cout << endl;
-	remove_nodde(t.root, 17);
-	print_tree(t.root);
-	cout << endl;
-	// cout << (t.root->value) << endl;
-	// delete t.root;
-	// cout << t.root->value << endl;
+	add_node(t.root, 25);
+	add_node(t.root, 14);
+	add_node(t.root, 36);
+	add_node(t.root, 3);
+	add_node(t.root, 17);
+	add_node(t.root, 20);
+	add_node(t.root, 19);
+	add_node(t.root, 21);
+	remove_node(t, t.root, 20);
+	// print_tree(t.root);
 
-	clear_tree(t.root);
-	// delete t;
+	// clear_tree(t.root);
 
 	return 0;
 }
