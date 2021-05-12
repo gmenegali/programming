88005e46de00aaf7d59f2f79562dbbe31bd8a0f1
gmenegali <guilherme.menegali@gmail.com>
2021-05-11 23:16:52 -0300

    702_A

---
+++ contests/702_A.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int n, x;
+        vi v;
+        cin >> n;
+ 
+        for(int i=0; i<n; i++){
+            cin >> x;
+            v.PB(x);
+        }
+
+        int count = 0;
+
+        for(int i=0; i<n-1; i++){
+            int f = v[i], s = v[i+1];
+            if(max(f,s) > 2 * min(f,s)){
+                if(f>s){
+                    while(f > 2*s){
+                        s *= 2;
+                        count++;
+                    }
+                }
+                else{
+                    while(s > 2*f){
+                        f *= 2;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        cout << count << endl;
+ 
+        t--;
+    }
+ 
+    return 0;
+}
