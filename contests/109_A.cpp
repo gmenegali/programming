1026bdcb81556bc614a1cbefc1fe6151d464582f
gmenegali <guilherme.menegali@gmail.com>
2021-05-16 23:29:53 -0300

    109_A

---
+++ contests/109_A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int k, complement;
+        cin >> k;
+        complement = 100 -k;
+
+        vi multiples = {2,2,5,5};
+
+        for(auto multiple : multiples){
+            if(k % multiple == 0) k /= multiple;
+            if(complement % multiple == 0) complement /= multiple;
+        }
+
+        cout << k+complement << endl;
+ 
+        t--;
+    }
+ 
+    return 0;
+}
