cf56189106e7b7aa4a42296311477763008c8e5d
gmenegali <guilherme.menegali@gmail.com>
2021-05-31 23:21:11 -0300

    611_A

---
+++ contests/611_A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int h, m;
+        cin >> h >> m;
+
+        if(m == 0) cout << 60*(24-h) << endl;
+        else cout << 60*(24-h-1) + (60-m) << endl;
+ 
+        t--;
+    }
+ 
+    return 0;
+}
