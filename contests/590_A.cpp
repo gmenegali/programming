c0479370fd13808fbc48ecddb61a9240e0fe288b
gmenegali <guilherme.menegali@gmail.com>
2021-06-12 14:00:00 +0100

    590_A

---
+++ contests/590_A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define len(s) (int) s.size()
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int n, x;
+        vi v;
+        cin >> n;
+
+        ll sum = 0;
+        for(int i=0; i<n; i++){
+            cin >> x;
+            sum += x;
+            v.PB(x);
+        }
+
+        int ans = sum/n + (sum % n != 0);
+        cout << ans << endl;
+
+        t--;
+    }
+ 
+    return 0;
+}
