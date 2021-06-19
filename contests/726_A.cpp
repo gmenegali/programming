9299bbd5fa1d10b72bf3d944724dd7fdfdb20dd2
gmenegali <guilherme.menegali@gmail.com>
2021-06-18 23:28:01 -0300

    726_A

---
+++ contests/726_A.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define len(s) (int) s.size()
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int n, x;
+        vi v;
+        cin >> n;
+
+        ll sum = 0;
+        for(int i=0; i<n; i++){
+            cin >> x;
+            sum += x;
+            v.PB(x);
+        }
+
+        if(sum == n) cout << 0 << endl;
+        else if(sum < n) cout << 1 << endl;
+        else cout << sum-n << endl;
+ 
+        t--;
+    }
+ 
+    return 0;
+}
