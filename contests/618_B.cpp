66fea57df139c0104a3b4d9bd21f2b7150b28710
gmenegali <guilherme.menegali@gmail.com>
2021-07-23 12:00:00 +0100

    618_B

---
+++ contests/618_B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define len(s) (int) s.size()
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int n, x;
+        vi v;
+        cin >> n;
+
+        int sum = 0;
+        int zeros = 0;
+        for(int i=0; i<n; i++){
+            cin >> x;
+            sum += x;
+            if(x == 0) zeros++;
+            v.PB(x);
+        }
+
+        int result = zeros;
+        if(sum+zeros == 0) result++;        
+
+        cout << result << endl;
+
+        t--;
+    }
+ 
+    return 0;
+}
