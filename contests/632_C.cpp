e449ee11cf2d83fb57dc6ce14ad351538457ab9b
gmenegali <guilherme.menegali@gmail.com>
2021-07-25 12:00:00 +0100

    632_C

---
+++ contests/632_C.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define len(s) (int) s.size()
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int n, x;
+    vi v;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> x;
+        v.PB(x);
+    }
+
+    printVec(v);
+
+    return 0;
+}
