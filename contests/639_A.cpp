409cc3bf878f040a50006afe7b85348552fe901d
gmenegali <guilherme.menegali@gmail.com>
2021-08-10 12:00:00 +0100

    639_A

---
+++ contests/639_A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define len(s) (int) s.size()
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+template <class vector_type>
+ 
+void printVec(vector<vector_type> &v){
+    for(int i=0; i < (int) v.size(); i++){
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+ 
+int main(){
+    int t;
+ 
+    cin >> t;
+    while(t){
+ 
+        int n, m;
+        cin >> n >> m;
+
+        if((n > 2 and m > 2) or (n==2 and m > 2) or (m==2 and n > 2) ){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+
+        t--;
+    }
+ 
+    return 0;
+}
